36f21f5817297160f9a4a48907ee3dd9824d9385
stefania <est.stefania.trianar@unimilitar.edu.co>
2026-04-24 12:51:22 -0500

    correccion de error de compilacion del canvas

--- script.js
+++ script.js
@@ -3,3 +3,15 @@
  * Asignatura: Introducción a la Computación Gráfica
  * Estudiante: Stefania Triana Rodriguez
  */
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+
+function plotPixel(ctx, x, y, color = "#000") {
+    ctx.fillStyle = color;
+    ctx.fillRect(Math.floor(x), Math.floor(y), 1, 1);
+}
+
+// prueba mínima
+plotPixel(ctx, 400, 300, "red");
+plotPixel(ctx, 401, 300, "red");
+plotPixel(ctx, 400, 301, "red");
